168ec2f23f581900ce39cda06b80397d5c60ce3a
adit-bala <adityarbala@gmail.com>
2022-08-19 17:34:45 -0400

    Refactor Metrics into Stopwatch Component & add backend logic for time

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import StopWatch from "../components/StopWatch";
 import "../styles/main.css";
 
 export default function Main() {
+  const start = 1660881600000; // new Date(2022, 7, 19)
+  const [time, setTime] = useState(0);
+  setInterval(() => {
+    setTime((new Date().getTime() - start) / 1000);
+  }, 1000);
   return (
     <>
       <header className="container">
@@ -13,37 +19,21 @@ export default function Main() {
         </p>
       </header>
       <div className="container flex">
-        <div className="progress">
-          <h3>Seconds</h3>
-        </div>
-        <div className="progress">
-          <h3>Minutes</h3>
-        </div>
-        <div className="progress">
-          <h3>Hours</h3>
-        </div>
-        <div className="progress">
-          <h3>Days</h3>
-        </div>
+        <StopWatch metric="Seconds" seconds={time} fraction={1} />
+        <StopWatch metric="Minutes" seconds={time} fraction={60} />
+        <StopWatch metric="Hours" seconds={time} fraction={3600} />
+        <StopWatch metric="Days" seconds={time} fraction={86400} />
       </div>
       <div className="container flex">
-        <div className="progress">
-          <h3>Weeks</h3>
-        </div>
-        <div className="progress">
-          <h3>Months</h3>
-        </div>
-        <div className="progress">
-          <h3>Years</h3>
-        </div>
+        <StopWatch metric="Weeks" seconds={time} fraction={604800} />
+        <StopWatch metric="Months" seconds={time} fraction={2592000} />
+        <StopWatch metric="Years" seconds={time} fraction={31556952} />
       </div>
-      <div> [insert clock] </div>
       <footer>
         <p>
           {" "}
-          Inspired by <a href="https://github.com/jakealbaugh">
-            @jakealbaugh
-          </a>{" "}
+          Heavily inspired by{" "}
+          <a href="https://github.com/jakealbaugh">@jakealbaugh</a>{" "}
         </p>
         <p> Special shoutout to my friend Oatmeal Olimar </p>
         <p>
